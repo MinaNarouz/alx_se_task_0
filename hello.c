f04c9bc2cc742e8f51d38df9e73bcb1444721c7e
minanady <mina.n.narouz@gmail.com>
2023-05-28 21:44:20 +0200

    solving errors in syntax

--- hello.c
+++ hello.c
@@ -2,8 +2,8 @@
 #include<string.h>
 
 int main(void){
-string text = "Hello from VIM";
-printf("This file altered by VIM, File text = %s \n", test"); 
+char* text = "Hello from VIM";
+printf("This file altered by VIM, File text = %s \n", text); 
 
 }
 
